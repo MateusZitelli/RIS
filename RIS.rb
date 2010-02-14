931146e9bb84be385a869f72031d8628d1a14ee6
MateusZitelli <zitellimateus@gmail.com>
2010-02-13 18:32:33 -0700

    Some bugs resolved.

--- RIS.rb
+++ RIS.rb
@@ -1,14 +1,15 @@
 require "RMagick"
 require "narray"
 include Magick
-$letras = %w[1 0]
+$letras = %w[0 1 \ ]
 $letras << " "
 
 class Letras
 	def initialize
 		puts "Quantidade de letras no eixo de maior tamanho:"
 		@@tamImagem = gets.chomp.to_i
-		@@img = Image.read("teste3.jpg")[0]
+		@@img = Image.read("gnu.png")[0]
+
 		if @@img.columns > @@img.rows
 			@eixoX = @@img.columns.to_f / @@tamImagem
 			@@img = @@img.resize(@@tamImagem,@@img.rows/@eixoX)
@@ -16,8 +17,9 @@ class Letras
 			@eixoY = @@img.rows.to_f / @@tamImagem
 			@@img = @@img.resize(@@img.columns/@eixoY,@@tamImagem)
 		end
+
 		@@img2 = @@img.export_pixels_to_str(0,0,@@img.columns,@@img.rows,"I",CharPixel)
-		@@img2R = @@img.export_pixels_to_str(0,0,@@img.columns,@@img.rows,"R",CharPixel)
+    @@img2R = @@img.export_pixels_to_str(0,0,@@img.columns,@@img.rows,"R",CharPixel)
 		@@img2G = @@img.export_pixels_to_str(0,0,@@img.columns,@@img.rows,"G",CharPixel)
 		@@img2B = @@img.export_pixels_to_str(0,0,@@img.columns,@@img.rows,"B",CharPixel)
 
@@ -28,25 +30,28 @@ class Letras
 
 		@@file = String.new
 		@@gc = Draw.new
+
 	end
+	def gerar(x=0)
+	  (@@img2R.size).times do |vez|
+			@@file << '
+' if vez % @@img.columns == 0 and vez != 0 and (x == 2 or x == 1)
+			@@file << $letras[(@@img2[vez]*($letras.size-1))/255] if x == 0 or x == 2
+			@@file << $letras[(@@img2[vez]*($letras.size-1))/255]*2 if x == 1
+		end
+  end
 
 	def pintar_cor
-	  (@@img2.size).times do |vez|
-			@@file << $letras[(@@img2[vez]*($letras.size-1))/255]
-		end
+	  gerar()
 		puts "Tamanho da fonte:"
 		@@tamFonte = gets.chomp.to_i
 		@@gc.pointsize(@@tamFonte)
-		times = @@tamFonte
-		@locLy=@@tamFonte
 		@@gc.fill("white")
-		puts @@file.size
-		puts @@img2.size
 		(@@img2.size).times do |x|
 		  @@gc.fill("rgb("+@@img2R[x].to_s+","+@@img2G[x].to_s+","+@@img2B[x].to_s+")")
-		  @@gc.text(x%@@img.columns*(@@tamFonte-@@tamFonte/5.0),x/@@img.columns*(@@tamFonte-@@tamFonte/5.0),@@file[x].chr)
+		  @@gc.text(x%@@img.columns*(@@tamFonte-@@tamFonte/5),x/@@img.columns*(@@tamFonte-@@tamFonte/5),@@file[x].chr)
     end
-		@imgFinal = Image.new(@@img.columns*(@@tamFonte-@@tamFonte/5.0),@@img.rows*(@@tamFonte-@@tamFonte/5.0)){self.background_color = "black"}
+		@imgFinal = Image.new(@@img.columns*(@@tamFonte-@@tamFonte/5),@@img.rows*(@@tamFonte-@@tamFonte/5)){self.background_color = "black"}
 		@@gc.draw(@imgFinal)
 		@imgFinal.display
 		puts "Deseja salvar: (y/n)"
@@ -56,18 +61,15 @@ class Letras
 	end
 
 	def pintar_pb
+	  gerar()
 		puts "Tamanho da fonte:"
 		@@tamFonte = gets.chomp.to_i
 		@@gc.pointsize(@@tamFonte)
-		times = @@tamFonte
-		@@gc.fill("black")
-		#@@file.each_line do |line|           ##################
-		#	@@gc.text(0,times,line)             #Create the image#
-		#	times += @@tamFonte #pula uma linha #linha per linha #
-		#end                                  ##################
-		@@gc.text(0,@@tamFonte,@@file)
-		@imgFinal = Image.new(@@img.columns*@@tamFonte,@@img.rows*@@tamFonte)
-		@imgFinal.background_color = "white"
+		@@gc.fill("white")
+		(@@img2.size).times do |x|
+		  @@gc.text(x%@@img.columns*(@@tamFonte-@@tamFonte/5),x/@@img.columns*(@@tamFonte-@@tamFonte/5),@@file[x].chr)
+    end
+		@imgFinal = Image.new(@@img.columns*(@@tamFonte-@@tamFonte/5),@@img.rows*(@@tamFonte-@@tamFonte/5)){self.background_color = "black"}
 		@@gc.draw(@imgFinal)
 		@imgFinal.display
 		puts "Deseja salvar: (y/n)"
@@ -77,11 +79,7 @@ class Letras
 	end
 
 	def escrever
-	  (@@img2R.size).times do |vez|
-			@@file << '
-' if vez % @@img.columns == 0 and vez != 0
-			@@file << $letras[(@@img2[vez]*($letras.size-1))/255]*2
-		end
+	  gerar(1)
 		puts "Nome do arquivo:"
 		@texto = File.new(gets.chomp,"w")
 		@@file.each_line do |line|
@@ -91,10 +89,4 @@ class Letras
 end
 
 teste = Letras.new
-if varteste == "drawbw"
-	teste.pintar_pb
-elsif varteste == "drawcolor"
-	teste.pintar_cor
-elsif varteste == "write"
-  teste.escrever
-end
+teste.pintar_pb
